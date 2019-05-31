2e9c3bffe141640d32c3729720ec05eeb97ec3bc
vince1171 <franchomme.vincent@gmail.com>
2019-05-31 16:56:46 +0200

    [Ubuntu Touch] temporary add missing gps blob (not perfect)

--- device.mk
+++ device.mk
@@ -343,7 +343,25 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ubuntu/android.conf:system/halium/etc/ubuntu-touch-session.d/android.conf \
     $(LOCAL_PATH)/ubuntu/bluetooth-touch-oneplus3.conf:system/halium/etc/init/bluetooth-touch-android.conf \
     $(LOCAL_PATH)/ubuntu/touch.pa:system/halium/etc/pulse/touch.pa \
-    $(LOCAL_PATH)/ubuntu/anbox-tool:system/halium/usr/bin/anbox-tool
+    $(LOCAL_PATH)/ubuntu/anbox-tool:system/halium/usr/bin/anbox-tool \
+    $(LOCAL_PATH)/ubuntu/hciattach:system/bin/hciattach \
+    $(LOCAL_PATH)/ubuntu/libs/libdataitems.so:system/lib/libdataitems.so \
+    $(LOCAL_PATH)/ubuntu/libs/libdrplugin_client.so:system/lib/libdrplugin_client.so \
+    $(LOCAL_PATH)/ubuntu/libs/libDRPlugin.so:system/lib/libDRPlugin.so \
+    $(LOCAL_PATH)/ubuntu/libs/libevent_observer.so:system/lib/libevent_observer.so \
+    $(LOCAL_PATH)/ubuntu/libs/libI420colorconvert.so:system/lib/libI420colorconvert.so \
+    $(LOCAL_PATH)/ubuntu/libs/libizat_core.so:system/lib/libizat_core.so \
+    $(LOCAL_PATH)/ubuntu/libs/liblbs_core.so:system/lib/liblbs_core.so \
+    $(LOCAL_PATH)/ubuntu/libs/libloc_adapter.so:system/lib/libloc_adapter.so \
+    $(LOCAL_PATH)/ubuntu/libs/libloc_api-rpc-qc.so:system/lib/libloc_api-rpc-qc.so \
+    $(LOCAL_PATH)/ubuntu/libs/libloc_api_v02.so:system/lib/libloc_api_v02.so \
+    $(LOCAL_PATH)/ubuntu/libs/liblocationservice_glue.so:system/lib/liblocationservice_glue.so \
+    $(LOCAL_PATH)/ubuntu/libs/liblocationservice.so:system/lib/liblocationservice.so \
+    $(LOCAL_PATH)/ubuntu/libs/libloc_ds_api.so:system/lib/libloc_ds_api.so \
+    $(LOCAL_PATH)/ubuntu/libs/liblowi_client.so:system/lib/liblowi_client.so \
+    $(LOCAL_PATH)/ubuntu/libs/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
+    $(LOCAL_PATH)/ubuntu/libs/libulp2.so:system/lib/libulp2.so
+
 # UBPorts
  PRODUCT_PACKAGES += \
      libubuntu_application_api \
