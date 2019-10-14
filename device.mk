9345a807b43662f3f576a801658c783356e24fe9
vince1171 <franchomme.vincent@gmail.com>
2019-10-14 14:54:31 +0200

    env: override default env

--- device.mk
+++ device.mk
@@ -348,7 +348,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ubuntu/anbox-tool:system/halium/usr/bin/anbox-tool \
     $(LOCAL_PATH)/ubuntu/hciattach:system/bin/hciattach \
     $(LOCAL_PATH)/ubuntu/usr.bin.media-hub-server:system/halium/etc/apparmor.d/local/usr.bin.media-hub-server \
-    $(LOCAL_PATH)/ubuntu/base:system/halium/etc/apparmor.d/abstractions/base
+    $(LOCAL_PATH)/ubuntu/base:system/halium/etc/apparmor.d/abstractions/base \
+    $(LOCAL_PATH)/ubuntu/environment:system/halium/etc/environment
 
 # UBPorts
  PRODUCT_PACKAGES += \
