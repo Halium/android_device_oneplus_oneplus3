7ea838810d9c76b50ca6b805c6e6cfa2772cd9db
Ernest <ernesst@posteo.net>
2020-02-01 23:15:23 -0600

    update wifi_restart script output

--- ubuntu/wlan_restart.py
+++ ubuntu/wlan_restart.py
@@ -5,6 +5,7 @@ import os.path
 content_file = '/sys/devices/soc/600000.qcom,pcie/pci0000:00/0000:00:00.0/0000:01:00.0/net/wlp1s0/address'
 
 if os.path.isfile(content_file) :
-    print("ok")
+    print("Wlan already activated")
 else:
     subprocess.Popen("echo sta > /sys/module/wlan/parameters/fwpath", shell=True)
+    print("Wifi card set up for wlan activation")
